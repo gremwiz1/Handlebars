242e9f9e9ecf73a78e8a2876130c6cfa548eacac
Gremwiz1 <gremwiz@yandex.ru>
2021-08-20 01:01:28 +0300

    Added layout false

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.post("/api/todolist", celebrate({
 }), (req, res, next) => {
   const { name } = req.body;
   Todolist.create({ name })
-    .then((item) => res.status(200).render("item", { layout: false }, { item }))
+    .then((item) => res.status(200).render("item", { item, layout: false }))
     .catch((err) => {
       if (err.name === "ValidationError") {
         next(new BadRequestError(`Переданы не корректные данные: ${err}`));
